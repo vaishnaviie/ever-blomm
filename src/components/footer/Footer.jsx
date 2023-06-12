aa348dcdc680156b70e68e64d44836e4bf412777
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2023-06-12 18:42:45 +0530

    refactor footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { ImGithub } from "react-icons/im";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>github</div>;
+  return (
+    <div className="footer_container">
+      <div className="footer_sub_container">
+        <ImGithub style={{ color: "black ", cursor: "pointer" }} />
+        <BsTwitter style={{ color: "black ", cursor: "pointer" }} />
+        <SiLinkedin style={{ color: "black ", cursor: "pointer" }} />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
