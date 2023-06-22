89b910e17cea523492290d1ce3a168ab2496bb61
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2023-06-22 15:48:44 +0530

    fixed an error

---
+++ src/pages/orderPlaced/OrderPlaced.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrderPlaced = () => {
+  return (
+    <div style={{ height: "89vh", textAlign: "center" }}>
+      <h1>Order Placed</h1>
+    </div>
+  );
+};
+
+export default OrderPlaced;
